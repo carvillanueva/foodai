b6f50bcce142b89654dbf63bfaa0199d9d2533de
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-05-29 19:43:38 -0400

    file restructure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipeGenerateComponent } from './pages/recipe-generate/recipe-generate.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'generate', component: RecipeGenerateComponent}
     // other routes...
   ];
---
+++ src/app/pages/recipe-generate/recipe-generate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-generate',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-generate.component.html',
+  styleUrl: './recipe-generate.component.css'
+})
+export class RecipeGenerateComponent {
+
+}
